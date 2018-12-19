08471abd4c4b982026ee874ba9b08a183cf32a33
Dozi0116 <oi.dozi0116@gmail.com>
2018-12-19 23:30:37 +0900

    [fix]真ん中にスコアが配置されるように修正

--- js/class/scene.js
+++ js/class/scene.js
@@ -150,7 +150,7 @@ class GameoverScene extends Scene {
     this.gameoverLogo = new GameoverLogo(this);
     this.addChild(this.gameoverLogo);
 
-    this.scoreInfo = new GeneralText(this, 'score: ' + this.root.score, this.root.width / 2, this.root.height / 2, '100px mplus');
+    this.scoreInfo = new GeneralText(this, 'score: ' + this.root.score, this.root.width / 2, this.root.height / 2, '100px mplus', undefined, 'center', 'middle');
     this.addChild(this.scoreInfo);
 
     this.endButton = new ToTitleButton(this, this.root.width / 2, this.root.height / 4 * 3);
